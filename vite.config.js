89edd298d7fb8d9a1ee8d4bf2a5d6e8ca7b23fd7
Christian <c.espinoza.code@gmail.com>
2023-06-12 15:28:42 -0500

    env variables used in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig((command, mode) => {
   const port = 3000
+  
+  console.log(command, mode);
+  const env = loadEnv(mode, process.cwd())
+
+  console.log(env);
+  console.log(env.VITE_NAME);
+
+  if (mode === 'development') {
+    console.log('Modo Desarrollo');
+  } else {
+    console.log('Modo Produccion');
+  }
   
   return {
     server: {
